7add982e499f12a6b7b4e8164358012751523a70
Dominick Campanile <Dominick@Dominicks-MacBook-Air.local>
2021-10-15 14:02:21 -0500

    added register

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, redirect, flash
+from app.forms import UserInfoForm
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = UserInfoForm()
+    if register_form.validate_on_submit():
+        return redirect(url_for('index'))   
+    flash('That username is already taken, please try again.')
+    return render_template('register.html', form=register_form)
